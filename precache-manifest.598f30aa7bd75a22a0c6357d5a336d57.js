e7e7d4800f5523b6ffe85d5d0029affd7351821c
Mateusz Hackiewicz <M.Hackiewicz@tt.com.pl>
2018-10-26 13:04:51 +0200

    first commit

---
+++ precache-manifest.598f30aa7bd75a22a0c6357d5a336d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e0e33769c5bf8245699",
+    "url": "/performance-testing-react-app/static/js/runtime~main.4e0e3376.js"
+  },
+  {
+    "revision": "b5909961f25c5b72d6ed",
+    "url": "/performance-testing-react-app/static/js/main.b5909961.chunk.js"
+  },
+  {
+    "revision": "e024e23f9e6dafcd9ae9",
+    "url": "/performance-testing-react-app/static/js/1.e024e23f.chunk.js"
+  },
+  {
+    "revision": "b5909961f25c5b72d6ed",
+    "url": "/performance-testing-react-app/static/css/main.40bb0831.chunk.css"
+  },
+  {
+    "revision": "e024e23f9e6dafcd9ae9",
+    "url": "/performance-testing-react-app/static/css/1.9fb286e6.chunk.css"
+  },
+  {
+    "revision": "1b6bb410b84be359d9c7ae9fcc1c392a",
+    "url": "/performance-testing-react-app/index.html"
+  }
+];
